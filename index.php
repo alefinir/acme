0710feffeb4047cdec86020937b188fb54f52fa8
alefinir <alefinir@byui.edu>
2017-09-19 13:11:47 -0300

    adding google fonts pairing and some meta tags

--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="ACME Foods">
+	<meta name="author" content="Team 4 -- CIT 336 -- BYUI">
+	<link href="https://fonts.googleapis.com/css?family=Bangers|Lora" rel="stylesheet"> 
+	<link rel="stylesheet" type="text/css" href="styles/main.css">
+	<link rel="stylesheet" type="text/css" href="styles/normalize.css">
+	<title>ACME</title>
 </head>
 <body>
-
+	<header>
+		<nav>		
+		</nav>
+	</header>
+	<section>
+		<article>
+			<h1>ACME FOODS</h1>
+			<p>..all is good</p>
+		</article>
+	</section>
+	<section>
+		<article>
+		</article>
+	</section>
+	<aside>	
+	</aside>
+	<footer>		
+	</footer>
 </body>
 </html>
