72e5fc56b80aca38740cbe73db1bbf42308c3443
alefinir <alefinir@byui.edu>
2017-11-03 17:04:53 -0300

    add sessions login

--- common/header.php
+++ common/header.php
@@ -4,13 +4,30 @@
 		<img class="logo-small" src="/acme/images/site/logo-small.gif" alt="Logo ACME Foods CellPhone"> 
 	</div>
 	<div class="account">
-		<?php if(isset($cookieFirstname)){
-  			echo "<span id=".'"welcomeName"'.">Welcome $cookieFirstname </span>";
-		} ?> 
-		<a href="/acme/accounts/index.php">
-			<img class="img-acc" src="/acme/images/site/account-small.gif" alt="Logo ACME Foods"> 
-		</a>
-		<h3>My Account</h3>
+		<?php 
+			$logueado=0;
+			$welcome="";
+
+		if($logueado){
+  				echo "<h3>Logout</h3>";
+			}
+			else{
+
+			if(isset($cookieFirstname)){
+  					$welcome="<span id=".'"welcomeName"'.">Welcome $cookieFirstname</span>";
+			} 
+				echo "$welcome";
+				echo"
+				<a href=\"/acme/accounts/index.php\">
+					<img class=\"img-acc\" src=\"/acme/images/site/account-small.gif\" alt=\"Logo ACME Foods\"> 
+				</a>
+				<h3>My Account</h3>";
+
+			}
+
+
+		?> 
+
 	</div>
 </div> 
 <nav class="navigation">
